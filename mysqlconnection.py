e23a526aff1425bc4d3fdff34e4b7d3b800499ac
isaiah <daezyworld@gmail.com>
2021-05-29 11:42:20 +0100

    handled requests

--- mysqlconnection.py
+++ mysqlconnection.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
 
 
@@ -8,8 +8,8 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password'
 app.config['MYSQL_DB'] = 'flask'
-
 mysql = MySQL(app)
+
 @app.route("/")
 def Home():
     cur = mysql.connection.cursor()
@@ -18,6 +18,19 @@ def Home():
     cur.close()
 
     return render_template('index.html', data = fetchdata)
+
+@app.route('/form', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['users_name']
+        lastName = details['users_email']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users(users_name, users_email) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('form.html')
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port="5001", debug = True)
