03dd6cb495c5c064ff801a30b7a1904aff16bb75
isaiah <daezyworld@gmail.com>
2021-06-11 17:43:02 +0100

    created request functionality

--- views.py
+++ views.py
@@ -1,42 +1,7 @@
-from os import name
-from flask import Flask, jsonify, json, request, make_response
-import uuid
-from werkzeug.security import generate_password_hash, check_password_hash
-import jwt
+from flask import jsonify, json, request
 
 from core import app
-from models import db, User, LogLag
-
-# creating new users
-@app.route('/register', methods=['POST', "GET"])
-def create_user():
-    data = json.loads(request.data, strict=False)
-    hashed_password = generate_password_hash(data['password'], method='sha256')
-    new_user = User(token=str(uuid.uuid4()),
-                    name=data['name'], password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({"message": "New User Created"})
-
-# Login user
-@app.route('/login', methods=['POST'])
-def login():
-    auth = request.authorization
-
-    if not auth or not auth.username or not auth.password:
-        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
-
-    user = User.query.filter_by(name=auth.username).first()
-
-    if not user:
-        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
-
-    if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'token': user.token}, app.config['SECRET_KEY'])
-
-        return jsonify({'token': token})
-
-    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+from models import db, LogLag
 
 
 @app.route('/submit', methods=['POST'])
