6ec56fc06e459a47e365d2edd384433deefbcb0a
Wobin <nathanielmusa3@gmail.com>
2021-06-28 19:53:13 +0100

    changed the models

--- models.py
+++ models.py
@@ -5,22 +5,36 @@ from flask_migrate import Migrate
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 # user table
-
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    token = db.Column(db.String(50), unique=True)
-    name = db.Column(db.String(50))
-    password = db.Column(db.String(128))
-    admin = db.Column(db.Boolean)
+    company_Name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(20), nullable=False)
+    files = db.relationship('File', backref='author', lazy=True)
 
-class LogLag(db.Model):
+    def __repr__(self):
+        return f"User('{self.company_Name}', '{self.email}' )"
+
+
+#File Table
+class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    logitude = db.Column(db.Float(128))
-    latitude = db.Column(db.Float(128))
+    file = db.Column(db.LargeBinary, nullable=False)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    file_Metadatas = db.relationship('File_metadata', backref='uploadedFile', lazy=True)
 
-    def __init__(self, logitude, latitude):
-        self.logitude = logitude
-        self.latitude = latitude
+    def __repr__(self):
+        return f"File('{self.id}', )"
+
+# Metadata table
+class File_metadata(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    file_Name = db.Column(db.String(20), nullable=False)
+    file_Type = db.Column(db.String(10), nullable=False)
+    file = db.Column(db.Integer, db.ForeignKey('file.id'), nullable=False)
+
+    def __repr__(self):
+        return f"File_metadata({self.file_Name}, {self.file_Type})"
 
--- views.py
+++ views.py
@@ -1,30 +1,31 @@
 from flask import jsonify, json, request
 
 from core import app
-from models import db, LogLag
+# from models import db
+# , LogLag
 
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    data = json.loads(request.data, strict=False)
-    new_loglat = LogLag(logitude=data['logitude'], latitude=data['latitude'])
-    db.session.add(new_loglat)
-    db.session.commit()
-    return jsonify({"message": "Logititude and Latitude submitted successfully"})
+# @app.route('/submit', methods=['POST'])
+# def submit():
+#     data = json.loads(request.data, strict=False)
+#     new_loglat = LogLag(logitude=data['logitude'], latitude=data['latitude'])
+#     db.session.add(new_loglat)
+#     db.session.commit()
+#     return jsonify({"message": "Logititude and Latitude submitted successfully"})
     
-@app.route('/retrieve', methods=['GET'])
-def getLogLat():
-    location = []
-    data = json.loads(request.data, strict=False)
-    resultt = LogLag.query.filter_by(logitude=data['logitude'], latitude=data['latitude'])
-    for log in resultt:
-        logi = log.logitude
-        lati = log.latitude
-        result = {
-            "Requested logitude" : logi,
-            "Requested latitude ": lati
-        }
-        location.append(result)
-    return jsonify( location)
+# @app.route('/retrieve', methods=['GET'])
+# def getLogLat():
+#     location = []
+#     data = json.loads(request.data, strict=False)
+#     resultt = LogLag.query.filter_by(logitude=data['logitude'], latitude=data['latitude'])
+#     for log in resultt:
+#         logi = log.logitude
+#         lati = log.latitude
+#         result = {
+#             "Requested logitude" : logi,
+#             "Requested latitude ": lati
+#         }
+#         location.append(result)
+#     return jsonify(location)
     
     
